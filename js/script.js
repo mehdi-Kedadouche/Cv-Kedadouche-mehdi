511802cc780aecb7fdd79d364467869c5d739a5e
mehdi-kedadouche <mehdi.kedadouche@hotmail.fr>
2023-06-22 21:28:31 +0200

    mise à jour

--- js/script.js
+++ js/script.js
@@ -1,36 +1,23 @@
-console.log("ok");
-// boutton  Black
 
-  let button = document.querySelector('.myButton')
-  console.log(button);
-  let cibleBody = document.querySelector('body');
-  console.log(cibleBody);
-  let cvContainer = document.querySelector('.cv-container')
+  function changeBody() {
+    let body = document.querySelector('body');
+    let cvContainer = document.querySelector('.cv-container');
 
-  function darkBody() {
+    body.classList.toggle('bodySombre');
+    cvContainer.classList.toggle('cv-container--dark-Mode')
+ 
+ }
+ function clickBody(){
+ 
+    let button = document.querySelector('.slider');
 
-      cibleBody.classList.remove('bodyClair');
-      cibleBody.classList.add('bodySombre');
-      cvContainer.classList.remove('cv-container')
-      cvContainer.classList.add('cv-container--dark-Mode')
-      console.log(button);
+ 
+    button.addEventListener('click', changeBody )
+ 
+ }
+ 
+ clickBody();
 
-  }
+ 
 
-  button.addEventListener('click', darkBody);
 
-  // Boutton Clair
-
-  let whiteButton = document.querySelector(".whiteButton")
-  console.log(whiteButton);
-
-  function whiteBody() {
-
-      cibleBody.classList.remove('bodySombre');
-      cibleBody.classList.add('bodyClair');
-      cvContainer.classList.add('cv-container')
-      cvContainer.classList.remove('cv-container--dark-Mode')
-      console.log(whiteButton);
-  }
-
-  whiteButton.addEventListener('click', whiteBody);
